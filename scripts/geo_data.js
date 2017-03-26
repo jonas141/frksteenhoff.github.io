b923619dce16cccd0e75ad75a5e79d7cce6f4787
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2017-03-26 17:18:08 +0200

    Cleaned up the code
    Minimized code duplication.

--- scripts/geo_data.js
+++ scripts/geo_data.js
@@ -15,7 +15,7 @@
   var centroids_k5;
   var centroids_k6;
 
-      // Projection variable
+      // Projection variable - mercator map view
       var projection = d3.geo.mercator()
                              .center([-122.433701, 37.767683])
                              .translate([w/2, h/2])
@@ -23,10 +23,10 @@
 
       // Path variable
       var path = d3.geo.path()
-                       .projection(projection);
+                    .projection(projection);
 
       //Create SVG element
-		var svg = d3.select("#geo_data")
+      var svg = d3.select("#geo_data")
 					.append("svg")
 					.attr("width", w)
 					.attr("height", h);
@@ -43,57 +43,22 @@
 
       // Loading crime data
       d3.json("model_data.json", function(cluster_data) {
-        	// storing data
+        	// Storing data
+        	// Datapoints for the clusters
       		datapoints_k2 = cluster_data.datapoints.k2;
-          datapoints_k3 = cluster_data.datapoints.k3;
+            datapoints_k3 = cluster_data.datapoints.k3;
         	datapoints_k4 = cluster_data.datapoints.k4;
         	datapoints_k5 = cluster_data.datapoints.k5;
         	datapoints_k6 = cluster_data.datapoints.k6;
+        	// Centroids
         	centroids_k2  = cluster_data.centroids.k2;
         	centroids_k3  = cluster_data.centroids.k3;
         	centroids_k4  = cluster_data.centroids.k4;
-          centroids_k5  = cluster_data.centroids.k5;
+            centroids_k5  = cluster_data.centroids.k5;
         	centroids_k6  = cluster_data.centroids.k6;
 
-        // Drawing data
-        svg.selectAll("k2data")
-           .data(datapoints_k2)
-           .enter()
-           .append("circle")
-           .attr("class", "data points")
-           .attr("cx", function(d) {
-                   return projection([d.lon, d.lat])[0];
-           })
-           .attr("cy", function(d) {
-                   return projection([d.lon, d.lat])[1];
-           })
-           .attr("r", 2)
-           .style("fill", function(d) {
-                  if (d.Cluster == 1) {	
-                    return "Yellow";
-                  } else {
-                    return "Red";
-                  }
-           })
-           .style("opacity", 0.6);
-
-           // Drawing data
-           svg.selectAll("k2centroids")
-              .data(centroids_k2)
-              .enter()
-              .append("circle")
-              .attr("class", "centroids")
-              .attr("cx", function(d) {
-                return projection([d.lon, d.lat])[0];
-              })
-              .attr("cy", function(d) {
-                return projection([d.lon, d.lat])[1];
-              })
-              .attr("r", 5)
-              .style("fill", "none")
-              .style("stroke", "black")
-              .style("stroke-width","2")
-              .style("opacity", 1);
+      // Setting initial number of clusters (showing as page is loaded first time)
+      updateData(2);
       });
 
 function updateData(noOfClusters) {
